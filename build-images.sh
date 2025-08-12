3954a2f8188e2a6c4d1cca6a3adc77595a314a71
Alexey Charkov <alchark@gmail.com>
2025-08-12 23:44:47 +0400

    Add timestamp to built images and their hostnames
    Signed-off-by: Alexey Charkov <alchark@gmail.com>

--- build-images.sh
+++ build-images.sh
@@ -2,6 +2,8 @@
 : "${UBOOT_DIR:=prebuilt/u-boot}"
 : "${OUT:=out}"
 
+TIMESTAMP=`date -u '+%Y%m%d-%k%M'`
+
 if [ -c /dev/kvm -a -w /dev/kvm ]; then
 	DEBOS="debos"
 else
@@ -10,14 +12,14 @@ fi
 
 mkdir -p "$OUT"
 
-$DEBOS --artifactdir="$OUT" debian-rk3576.yaml
+$DEBOS --artifactdir="$OUT" -t timestamp:"$TIMESTAMP" debian-rk3576.yaml
 
 for i in `basename -a "$UBOOT_DIR"/*`; do
-	cp "$OUT"/debian-nobootloader.img "$OUT"/debian-"$i".img
-	dd if="$UBOOT_DIR"/"$i"/u-boot-rockchip.bin of="$OUT"/debian-"$i".img seek=64 conv=notrunc
-	bmaptool create -o "$OUT"/debian-"$i".img.bmap "$OUT"/debian-"$i".img
-	pigz -f "$OUT"/debian-"$i".img
+	cp "$OUT"/debian-nobootloader-"$TIMESTAMP".img "$OUT"/debian-"$i"-"$TIMESTAMP".img
+	dd if="$UBOOT_DIR"/"$i"/u-boot-rockchip.bin of="$OUT"/debian-"$i"-"$TIMESTAMP".img seek=64 conv=notrunc
+	bmaptool create -o "$OUT"/debian-"$i"-"$TIMESTAMP".img.bmap "$OUT"/debian-"$i"-"$TIMESTAMP".img
+	pigz -f "$OUT"/debian-"$i"-"$TIMESTAMP".img
 done
 
-bmaptool create -o "$OUT"/debian-nobootloader.img.bmap "$OUT"/debian-nobootloader.img
-pigz -f "$OUT"/debian-nobootloader.img
+bmaptool create -o "$OUT"/debian-nobootloader-"$TIMESTAMP".img.bmap "$OUT"/debian-nobootloader-"$TIMESTAMP".img
+pigz -f "$OUT"/debian-nobootloader-"$TIMESTAMP".img
