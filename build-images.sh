9142461fa1cc88030b794fd6704d4177c74f9eb8
Alexey Charkov <alchark@gmail.com>
2025-11-28 15:44:58 +0400

    Generate images for both 512 and 4096 byte sector sizes
    Use 4096 for UFS (and likely NVME), 512 for SD/eMMC
    
    Signed-off-by: Alexey Charkov <alchark@gmail.com>

--- build-images.sh
+++ build-images.sh
@@ -33,18 +33,21 @@ rm -rf prebuilt/linux_tmp
 mkdir -p prebuilt/linux_tmp
 cp "$LINUX_OUT"/* prebuilt/linux_tmp/
 
-$DEBOS --artifactdir="$IMG_OUT" -t buildid:"$BUILD_ID" -t gitinfo:"$GIT_INFO" -t kerneldir:prebuilt/linux_tmp debian-rk3576.yaml
-
-[ $? -ne 0 ] && echo "debos didn't run successfully, aborting" && exit 1
-
+$DEBOS --artifactdir="$IMG_OUT" -t buildid:"$BUILD_ID" -t gitinfo:"$GIT_INFO" -t kerneldir:prebuilt/linux_tmp debian-rk3576-ospack.yaml
 rm -rf prebuilt/linux_tmp
 
+for s in 512 4096; do
+echo "Creating images for $s-byte sector size"
+$DEBOS --artifactdir="$IMG_OUT" -t buildid:"$BUILD_ID" -t sectorsize:"$s" debian-rk3576-img.yaml
+
 for i in `basename -a "$UBOOT_OUT"/*`; do
-	cp "$IMG_OUT"/debian-nobootloader-"$BUILD_ID".img "$IMG_OUT"/debian-"$i"-"$BUILD_ID".img
-	dd if="$UBOOT_OUT"/"$i"/u-boot-rockchip.bin of="$IMG_OUT"/debian-"$i"-"$BUILD_ID".img seek=64 conv=notrunc
-	bmaptool create -o "$IMG_OUT"/debian-"$i"-"$BUILD_ID".img.bmap "$IMG_OUT"/debian-"$i"-"$BUILD_ID".img
-	pigz -f "$IMG_OUT"/debian-"$i"-"$BUILD_ID".img
+	echo "$i board:"
+	cp "$IMG_OUT"/debian-"$s"-nobootloader-"$BUILD_ID".img "$IMG_OUT"/debian-"$s"-"$i"-"$BUILD_ID".img
+	dd if="$UBOOT_OUT"/"$i"/u-boot-rockchip.bin of="$IMG_OUT"/debian-"$s"-"$i"-"$BUILD_ID".img seek=64 conv=notrunc
+	bmaptool create -o "$IMG_OUT"/debian-"$s"-"$i"-"$BUILD_ID".img.bmap "$IMG_OUT"/debian-"$s"-"$i"-"$BUILD_ID".img
+	pigz -f "$IMG_OUT"/debian-"$s"-"$i"-"$BUILD_ID".img
 done
 
-bmaptool create -o "$IMG_OUT"/debian-nobootloader-"$BUILD_ID".img.bmap "$IMG_OUT"/debian-nobootloader-"$BUILD_ID".img
-pigz -f "$IMG_OUT"/debian-nobootloader-"$BUILD_ID".img
+bmaptool create -o "$IMG_OUT"/debian-"$s"-nobootloader-"$BUILD_ID".img.bmap "$IMG_OUT"/debian-"$s"-nobootloader-"$BUILD_ID".img
+pigz -f "$IMG_OUT"/debian-"$s"-nobootloader-"$BUILD_ID".img
+done
