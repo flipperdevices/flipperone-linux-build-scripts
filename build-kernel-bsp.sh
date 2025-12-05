2590f50b624d61977260a988b1f30f74720d0322
Alexey Charkov <alchark@gmail.com>
2025-12-05 22:33:09 +0400

    Add missing RK3576 config fragment from Rockchip defaults (BSP kernel)
    Signed-off-by: Alexey Charkov <alchark@gmail.com>

--- build-kernel-bsp.sh
+++ build-kernel-bsp.sh
@@ -60,7 +60,7 @@ fi
 
 pushd "$LINUX_DIR"
 make ARCH=arm64 CROSS_COMPILE="$CROSS_COMPILE" -j$(nproc) clean
-make ARCH=arm64 CROSS_COMPILE="$CROSS_COMPILE" -j$(nproc) defconfig rockchip_linux_defconfig
+make ARCH=arm64 CROSS_COMPILE="$CROSS_COMPILE" -j$(nproc) defconfig rockchip_linux_defconfig rk3576.config
 make ARCH=arm64 CROSS_COMPILE="$CROSS_COMPILE" -j$(nproc) bindeb-pkg
 popd
 
