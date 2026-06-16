456724867b9c2e4521acf538b1ac992c4cc67cdc
Alexey Charkov <alchark@flipper.net>
2026-06-16 13:53:25 +0400

    Add OP-TEE to bootloader builds
    Signed-off-by: Alexey Charkov <alchark@flipper.net>

--- build-uboot.sh
+++ build-uboot.sh
@@ -2,16 +2,22 @@
 : "${BOARD:=all}"
 : "${UBOOT_DIR:=src/u-boot}"
 : "${TFA_DIR:=src/tfa}"
+: "${TEE_DIR:=src/tee}"
 : "${RKBIN_DIR:=src/rkbin}"
 : "${KEEP_SRC:=no}"
 : "${UBOOT_OUT:=prebuilt/u-boot}"
 : "${CROSS_COMPILE:=aarch64-linux-gnu-}"
+: "${CROSS_COMPILE64:=aarch64-linux-gnu-}"
+: "${CROSS_COMPILE32:=arm-linux-gnueabihf-}"
 
 : "${UBOOT_GIT:=https://github.com/flipperdevices/u-boot.git}"
 : "${UBOOT_BRANCH:=rk3576}"
 
 : "${TFA_GIT:=https://github.com/ARM-software/arm-trusted-firmware.git}"
 : "${TFA_BRANCH:=master}"
+
+: "${TEE_GIT:=https://github.com/OP-TEE/optee_os.git}"
+: "${TEE_BRANCH:=master}"
 
 : "${RKBIN_GIT:=https://github.com/flipperdevices/rkbin}"
 : "${RKBIN_BRANCH:=master}"
@@ -20,7 +26,7 @@
 
 set -e
 
-for i in "$UBOOT_DIR" "$RKBIN_DIR" "$TFA_DIR"; do
+for i in "$UBOOT_DIR" "$RKBIN_DIR" "$TFA_DIR" "$TEE_DIR"; do
 	if [ -d "$i" ]; then
 		if [ x"$KEEP_SRC" = x"update" ]; then
 			pushd "$i"
@@ -35,6 +41,14 @@ done
 [ ! -d "$UBOOT_DIR" ] && git clone --depth 1 -b "$UBOOT_BRANCH" "$UBOOT_GIT" "$UBOOT_DIR"
 [ ! -d "$RKBIN_DIR" ] && git clone --depth 1 -b "$RKBIN_BRANCH" "$RKBIN_GIT" "$RKBIN_DIR"
 [ ! -d "$TFA_DIR" ] && [ x"$USE_BL31" = x"opensource" ] && git clone --depth 1 -b "$TFA_BRANCH" "$TFA_GIT" "$TFA_DIR"
+[ ! -d "$TEE_DIR" ] && git clone --depth 1 -b "$TEE_BRANCH" "$TEE_GIT" "$TEE_DIR"
+
+TEE=
+pushd "$TEE_DIR"
+make PLATFORM=rockchip-rk3576 CROSS_COMPILE32="$CROSS_COMPILE32" CROSS_COMPILE64="$CROSS_COMPILE64" -j$(nproc) clean
+make PLATFORM=rockchip-rk3576 CROSS_COMPILE32="$CROSS_COMPILE32" CROSS_COMPILE64="$CROSS_COMPILE64" CFG_USER_TA_TARGETS=ta_arm64 -j$(nproc)
+TEE=`realpath out/arm-plat-rockchip/core/tee.bin`
+popd
 
 ROCKCHIP_TPL=`realpath "$RKBIN_DIR"/bin/rk35/rk3576_ddr_*.bin | tail -n1`
 BL31=
@@ -42,7 +56,7 @@ BL31=
 if [ x"$USE_BL31" = x"opensource" ]; then
 	pushd "$TFA_DIR"
 	make PLAT=rk3576 -j$(nproc) clean
-	make PLAT=rk3576 -j$(nproc)
+	make PLAT=rk3576 BL32="$TEE" SPD=opteed -j$(nproc)
 	popd
 	BL31=`realpath "$TFA_DIR"/build/rk3576/release/bl31/bl31.elf`
 else
@@ -65,7 +79,8 @@ for i in $BOARDS; do
 	pushd "$UBOOT_DIR"
 	make -j$(nproc) CROSS_COMPILE="$CROSS_COMPILE" clean
 	make -j$(nproc) CROSS_COMPILE="$CROSS_COMPILE" "$i"-rk3576_defconfig rockchip-ramboot.config
-	make -j$(nproc) CROSS_COMPILE="$CROSS_COMPILE" BL31="$BL31" ROCKCHIP_TPL="$ROCKCHIP_TPL"
+	echo "CONFIG_OPTEE_TZDRAM_SIZE=0x2000000" >> .config
+	make -j$(nproc) CROSS_COMPILE="$CROSS_COMPILE" BL31="$BL31" ROCKCHIP_TPL="$ROCKCHIP_TPL" TEE="$TEE"
 	popd
 
 	rm -rf "$UBOOT_OUT"/"$i"
