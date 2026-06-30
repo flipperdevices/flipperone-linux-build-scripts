076761df425f1fac2282d098d0efbe2f7a4eebc0
Yury MonZon <yurymonzon@gmail.com>
2026-06-30 18:04:09 +0200

    Add stamping

--- build-rootfs-img.sh
+++ build-rootfs-img.sh
@@ -5,6 +5,13 @@
 
 set -e
 
+# Capture Git information
+[ -n "${GIT_HASH}" ] || GIT_HASH=$(git rev-parse --short HEAD 2>/dev/null || echo "unknown")
+[ -n "${GIT_BRANCH}" ] || GIT_BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --exact-match 2>/dev/null || echo "detached")
+[ -n "${GIT_MSG}" ] || GIT_MSG=$(git log -1 --pretty=format:"%s" 2>/dev/null | sed 's/[\"()]/\\&/g; s/'"'"'/\\&/g' || echo "unknown")
+[ -n "${GIT_INFO}" ] || GIT_INFO="${GIT_BRANCH}@${GIT_HASH}: ${GIT_MSG}"
+GIT_INFO=$(echo "$GIT_INFO" | tr -dc '[:alnum:][:space:]')
+
 if [ -c /dev/kvm -a -w /dev/kvm ]; then
 	# Have virtualization support, can use fakemachine (default, fast, safe)
 	DEBOS="debos -c $(nproc) -m 6Gb"
@@ -29,7 +36,7 @@ mkdir -p "$IMG_OUT"/linux_tmp
 cp -r "$LINUX_OUT"/* "$IMG_OUT"/linux_tmp
 
 echo "Creating the root FS image"
-$DEBOS --artifactdir="$IMG_OUT" -t imagesize:"$IMGSIZE" -t kerneldir:linux_tmp debian-rk3576-img.yaml
+$DEBOS --artifactdir="$IMG_OUT" -t imagesize:"$IMGSIZE" -t kerneldir:linux_tmp -t gitinfo:"$GIT_INFO" debian-rk3576-img.yaml
 sync "$IMG_OUT"/debian-nobootloader.img
 
 owner=$(stat -c %u "$IMG_OUT"/debian-nobootloader.img)
